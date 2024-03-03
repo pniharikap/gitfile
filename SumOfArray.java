fb2d46d62eded9635f640acad9824e23ef59436c
pniharika <patchaniharika@gmail.com>
2024-03-03 10:53:28 +0530

    the addition of elements in an array

---
+++ SumOfArray.java
@@ -0,0 +1,10 @@
+class SumOfArray{
+	public static void main(String[] args){
+		int a[] = {12,22,33,45,23};
+		int sum = 0;
+		for(int i=0;i<a.length;i++){
+			sum += a[i];
+		}
+		System.out.println("The sum of elements in an array is : "sum);
+	}
+}
